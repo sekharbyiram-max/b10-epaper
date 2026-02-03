e0175b49866204dba55f809314345684d8638d01
B10 Robot <robot@b10vartha.in>
2026-02-03 01:58:09 +0000

    Auto-published edition with SMART CROP

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const REPO_URL = "https://epaperb10vartha.in";
 // DATA 
 const editions = {
     // ROBOT_ENTRY_POINT
+    "03-02-2026": { pages: 5, pdf: "full.pdf" },
+
     "02-02-2026": { pages: 6, pdf: "full.pdf" },
 
     "01-02-2026": { pages: 6, pdf: "full.pdf" },
